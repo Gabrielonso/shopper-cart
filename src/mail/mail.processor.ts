445f676311149382fcbbf9950dbcb52b579bdd38
Gabrielonso <gabitgabriel1@gmail.com>
2024-07-28 10:17:07 +0100

    finishing up the project

--- src/mail/mail.processor.ts
+++ src/mail/mail.processor.ts
@@ -1,23 +1,26 @@
 import { Process, Processor } from '@nestjs/bull';
 import { Job } from 'bull';
 import { SendGridClient } from './sendgrid-client';
+import { ConfigService } from '@nestjs/config';
 
 @Processor('emails')
 export class EmailProcessor {
-  constructor(private readonly sendGridClient: SendGridClient) {}
+  constructor(
+    private readonly sendGridClient: SendGridClient,
+    private configService: ConfigService,
+  ) {}
   @Process('sendWelcomeEmail')
   async handleSendEmail(job: Job) {
     console.log('send email');
     const { email } = job.data;
     const msg = {
       to: email,
-      from: 'noreply@example.com',
-      subject: 'Welcome to Our Service',
+      from: this.configService.get<string>('MAIL_USER'),
+      subject: 'Welcome to Shopper Cart',
       text: 'Thank you for registering with our service.',
       html: '<strong>Thank you for registering with our service.</strong>',
     };
-    console.log('<<>>medsg>>', msg);
+
     const result = await this.sendGridClient.send(msg);
-    console.log(result);
   }
 }
--- src/mail/mail.service.ts
+++ src/mail/mail.service.ts
@@ -3,31 +3,34 @@ import { MailDataRequired } from '@sendgrid/mail';
 import { SendGridClient } from './sendgrid-client';
 import { InjectQueue } from '@nestjs/bull';
 import { Queue } from 'bull';
+import { ConfigService } from '@nestjs/config';
 
 @Injectable()
 export class MailService {
   constructor(
     private readonly sendGridClient: SendGridClient,
     @InjectQueue('emails') private emailQueue: Queue,
+    private configService: ConfigService,
   ) {}
 
-  // async sendWelcomeEmail(
-  //   recipient: string,
-  //   body = 'Welcome to Shopper Cart',
-  // ): Promise<void> {
-  //   const mail: MailDataRequired = {
-  //     to: recipient,
-  //     from: 'gabitgabriel1@gmail.com',
-  //     subject: 'Test email',
-  //     content: [{ type: 'text/plain', value: body }],
-  //   };
-  //   await this.sendGridClient.send(mail);
-  // }
-
-  async sendWelcomeEmail(email: string) {
-    console.log('<<>>', email);
-    const job = await this.emailQueue.add('sendWelcomeEmail', { email });
-    console.log(job, email);
-    return { jobId: job.id };
+  async sendWelcomeEmail(
+    receipientEmail: string,
+    body = 'Thank you for registering with our service.',
+  ): Promise<void> {
+    const mail: MailDataRequired = {
+      to: receipientEmail,
+      from: this.configService.get<string>('MAIL_USER'),
+      subject: 'Welcome to Shopper Cart',
+      content: [{ type: 'text/plain', value: body }],
+    };
+    await this.sendGridClient.send(mail);
   }
+
+  // async sendWelcomeEmail(receipientEmail: string) {
+  //   const job = await this.emailQueue.add('sendWelcomeEmail', {
+  //     email: receipientEmail,
+  //   });
+
+  //   return { jobId: job.id };
+  // }
 }
